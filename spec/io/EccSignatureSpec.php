9b83f75b97c5493e11e574bf811b15826f68dde6
Nikolas Martens <Nikolas.M@rtens.org>
2016-02-26 22:02:28 +0600

    ECC keys and signatures

--- spec/io/EccSignatureSpec.php
+++ spec/io/EccSignatureSpec.php
@@ -1,6 +1,71 @@
 <?php
 namespace spec\groupcash\php\io;
 
+use groupcash\php\key\EccKeyService;
+use groupcash\php\model\Signature;
+use rtens\scrut\Assert;
+use rtens\scrut\fixtures\ExceptionFixture;
+
+/**
+ * @property Assert assert <-
+ * @property ExceptionFixture try <-
+ * @property EccKeyService $ecc
+ */
 class EccSignatureSpec {
 
+    function before() {
+        if (getenv('SKIP_ECC')) {
+            $this->assert->incomplete('ECC spec skipped.');
+        }
+        $this->ecc = new EccKeyService();
+    }
+
+    function publicKeyOfInvalidPrivateKey() {
+        $this->try->tryTo(function () {
+            $this->ecc->publicKey('invalid');
+        });
+        $this->try->thenTheException_ShouldBeThrown('Invalid key.');
+    }
+
+    function signWithInvalidPrivateKey() {
+        $this->try->tryTo(function () {
+            $this->ecc->sign('foo', 'invalid');
+        });
+        $this->try->thenTheException_ShouldBeThrown('Invalid key.');
+    }
+
+    function verifyWithInvalidSignature() {
+        $this->try->tryTo(function () {
+            $this->ecc->verify('foo', new Signature('invalid', 'bar'));
+        });
+        $this->try->thenTheException_ShouldBeThrown('Invalid signature.');
+    }
+
+    function verifyWithInvalidPublicKey() {
+        $this->try->tryTo(function () {
+            $this->ecc->verify('foo', new Signature('invalid', 'foo#bar'));
+        });
+        $this->try->thenTheException_ShouldBeThrown('Invalid key.');
+    }
+
+    function verifyWithWrongKey() {
+        $key = $this->ecc->generatePrivateKey();
+        $wrong = $this->ecc->generatePrivateKey();
+
+        $signed = $this->ecc->sign('foo', $key);
+        $this->assert->not($this->ecc->verify('foo', new Signature($this->ecc->publicKey($wrong), $signed)));
+    }
+
+    function verifyWithWrongSignature() {
+        $key = $this->ecc->generatePrivateKey();
+
+        $signed = $this->ecc->sign('bar', $key);
+        $this->assert->not($this->ecc->verify('foo', new Signature($this->ecc->publicKey($key), $signed)));
+    }
+
+    function verifySignature() {
+        $key = $this->ecc->generatePrivateKey();
+        $signed = $this->ecc->sign('foo', $key);
+        $this->assert->isTrue($this->ecc->verify('foo', new Signature($this->ecc->publicKey($key), $signed)));
+    }
 }
